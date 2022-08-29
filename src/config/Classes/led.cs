505e433d94571db0143b0bd9ee196844fbea7b86
Eduardo Barreto <eduardopontobarreto@gmail.com>
2022-08-28 23:36:29 -0300

    feat: led class

---
+++ src/config/Classes/led.cs
@@ -0,0 +1,96 @@
+/**
+* @brief Gerencia um led
+*/
+
+public class led
+{
+    /**
+    * @brief Led a ser gerenciado.
+    */
+    private Light ledLight;
+    private long lastBlink;
+
+    /**
+    * @brief Construtor da classe.
+    *
+    * @param ledName: Nome do led 
+    */
+    public led(string ledName)
+    {
+        this.ledLight = Bot.GetComponent<Light>(ledName);
+    }
+
+    /**
+    * @brief Indica se o led está ligado.
+    *
+    */
+    public bool state
+    {
+        get => ledLight.Lit;
+    }
+
+    /**
+    * @brief Indica a cor atual do led;
+    *
+    */
+    public Color color
+    {
+        get => ledLight.Color;
+    }
+
+    /**
+    * @brief Liga o led com a cor especificada.
+    *
+    * @param color: Cor desejada (padrão vermelho)
+    */
+    public void on(string _color = "Vermelho")
+    {
+        ledLight.TurnOn(Color.ToColor(_color));
+    }
+
+    /**
+    * @brief Desliga o led.
+    *
+    */
+    public void off()
+    {
+        ledLight.TurnOff();
+    }
+
+    /**
+    * @brief Liga ou desliga o led.
+    *
+    * @param _state: Liga o led se verdadeiro
+    */
+    public void set(bool _state)
+    {
+        if (_state)
+            on();
+        else
+            off();
+    }
+
+    /**
+    * @brief Inverte o estado do led
+    *
+    */
+    public void toggle()
+    {
+        set(!state);
+    }
+
+    /**
+    * @brief Pisca o led.
+    *
+    * @param _time: Tempo de piscada (padrão 0.1s)
+    * @param _color: Cor do led (padrão branco)
+    */
+    public void blink(int _time = 100, string _color = "Branco")
+    {
+        if(timer.current < lastBlink + _time)
+            return;
+
+        toggle();
+        lastBlink = timer.current;
+    }
+}
